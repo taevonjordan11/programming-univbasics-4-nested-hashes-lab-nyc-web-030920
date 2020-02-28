97aa665e4de1c7b53642cbe3f7fb433072ae11b6
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-28 18:31:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -23,7 +23,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for][:languages]
+	programmer_hash[:grace_hopper][:known_for][:languages]
 	return programmer_hash
 
 	
